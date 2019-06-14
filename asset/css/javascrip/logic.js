d05b9968e95e03e6957ff446986acaa6af0812ce
Iman Gure <imangu99@gmail.com>
2019-06-13 21:55:08 -0700

    updating

---
+++ asset/css/javascrip/logic.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+
+    // Initialize Firebase
+    var firebaseConfig = {
+        apiKey: "AIzaSyByq9RPC4mKM0YxaLH96_nDgyzaC3B2U6g",
+        authDomain: "train-scheduler-912eb.firebaseapp.com",
+        databaseURL: "https://train-scheduler-912eb.firebaseio.com",
+        projectId: "train-scheduler-912eb",
+        storageBucket: "train-scheduler-912eb.appspot.com",
+        messagingSenderId: "542022980177",
+        appId: "1:542022980177:web:5ce80d7529578704"
+    };
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
+    // Capture Button Click
+    $("#addTrain").on("click", function (event) {
+        event.preventDefault();
+
+        // Grabbed values from text boxes
+        var trainName = $("#trainName").val().trim();
+        var destination = $("#destination").val().trim();
+        var firstTrain = $("#firstTrain").val().trim();
+        var freq = $("#interval").val().trim();
+
+        // Code for handling the push
+        database.ref().push({
+            trainName: trainName,
+            destination: destination,
+            firstTrain: firstTrain,
+            frequency: freq
+        });
+    });
+
+
+    // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
+    database.ref().on("child_added", function (childSnapshot) {
+
+        var newTrain = childSnapshot.val().trainName;
+        var newLocation = childSnapshot.val().destination;
+        var newFirstTrain = childSnapshot.val().firstTrain;
+        var newFreq = childSnapshot.val().frequency;
+
+        // First Time (pushed back 1 year to make sure it comes before current time)
+        var startTimeConverted = moment(newFirstTrain, "hh:mm").subtract(1, "years");
+
+        // Current Time
+        var currentTime = moment();
+
+        // Difference between the times
+        var diffTime = moment().diff(moment(startTimeConverted), "minutes");
+
+        // Time apart (remainder)
+        var tRemainder = diffTime % newFreq;
+
+        // Minute(s) Until Train
+        var tMinutesTillTrain = newFreq - tRemainder;
+
+        // Next Train
+        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        var catchTrain = moment(nextTrain).format("HH:mm");
+
+        // Display On Page
+        $("#all-display").append(
+            ' <tr><td>' + newTrain +
+            ' </td><td>' + newLocation +
+            ' </td><td>' + newFreq +
+            ' </td><td>' + catchTrain +
+            ' </td><td>' + tMinutesTillTrain + ' </td></tr>');
+
+        // Clear input fields
+        $("#trainName, #destination, #firstTrain, #interval").val("");
+        return false;
+    },
+        //Handle the errors
+        function (errorObject) {
+            console.log("Errors handled: " + errorObject.code);
+        });
+
+}); //end document ready
